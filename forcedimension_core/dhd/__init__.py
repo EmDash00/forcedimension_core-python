b336d61f2217db78f38d8d0614e673252a9f3622
Ember Chow <emberchow.businiess@iu.edu>
2023-09-18 15:34:08 -0700

    spacing style fix for import

--- forcedimension_core/dhd/__init__.py
+++ forcedimension_core/dhd/__init__.py
@@ -8,43 +8,9 @@ import forcedimension_core.containers as containers
 import forcedimension_core.runtime as _runtime
 from forcedimension_core.runtime import VersionTuple
 from forcedimension_core.typing import (
-    c_double_ptr, c_int_ptr, c_ushort_ptr,
-    Array, MutableArray
+    c_double_ptr, c_int_ptr, c_ushort_ptr, Array, MutableArray
 )
-from forcedimension_core.dhd.adaptors import (
-    DHDError,
-    DHDIOError,
-    DHDErrorCom,
-    DHDErrorDHCBusy,
-    DHDErrorNoDriverFound,
-    DHDErrorNoDeviceFound,
-    DHDErrorFeatureNotAvailable,
-    DHDErrorTimeout,
-    DHDErrorGeometry,
-    DHDErrorExpertModeDisabled,
-    DHDErrorNotImplemented,
-    DHDErrorMemory,
-    DHDErrorDeviceNotReady,
-    DHDErrorFileNotFound,
-    DHDErrorConfiguration,
-    DHDErrorInvalidIndex,
-    DHDErrorDeprecated,
-    DHDErrorNullArgument,
-    DHDErrorRedundantFail,
-    DHDErrorFeatureNotEnabled,
-    DHDErrorDeviceInUse,
-    DHDErrorArgument,
-    DHDErrorNoRegulation,
-    Handedness,
-    com_mode_str,
-    com_mode_from_str,
-    num_dof,
-    handedness,
-    handedness_str,
-    devtype_str,
-    velocity_estimator_mode_str,
-    errno_to_exception
-)
+
 from forcedimension_core.dhd.constants import (
     MAX_BUTTONS,
     MAX_DOF,
